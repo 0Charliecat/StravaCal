bee9756da2c1d721cb0a6c9a35be97d7b5df7f5d
Slovak_Cat <slovak_cat@boun.cr>
2021-10-22 13:15:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const { encode } = require('single-byte');
 const ics = require('ics')
 const keychain = require('./keychain.json')
 const { writeFile } = require('fs')
-const decoder = new TextDecoder('iso-8859-2');
+const decoder = new TextDecoder('utf-8');
+const JSONdb = require('simple-json-db');
+const pekelneRes = new JSONdb('pekelne-restauracie.json');
 
-// http://localhost:7890/cal/9977
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.end("im alive")
+    //res.end("StravaCal")
+  res.render('index');
 });
 
-app.get('/cal/:jidelna', (req, res) => {
+app.get('/cal/:jidelna',async (req, res) => {
+  console.log(req.get('user-agent'));
   let jidelna;
 
   if (!req.params.hasOwnProperty('jidelna')) {
@@ -24,8 +28,8 @@ app.get('/cal/:jidelna', (req, res) => {
     jidelna = req.params.jidelna
   }
   let tobereturned;
-  res.setHeader('Content-Type', 'text/calendar');
-  request({uri: `http://www.strava.cz/foxisapi/foxisapi.dll/istravne.istravne.process?xmljidelnickyA&zarizeni=${jidelna}&jazy%20k=SK&httphlavicka=A`}, function (error, response, body) {
+  await res.setHeader('Content-Type', 'text/calendar');
+  await request({uri: `http://www.strava.cz/foxisapi/foxisapi.dll/istravne.istravne.process?xmljidelnickyA&zarizeni=${jidelna}&jazy%20k=SK&httphlavicka=A`}, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       //body = windows1250.encode(body, { mode: 'replacement' });
@@ -33,11 +37,12 @@ app.get('/cal/:jidelna', (req, res) => {
       //console.log(decoder.decode(buf))
       parseString(decoder.decode(buf), function (err, result) {
         res.end(makeCal(result.VFPData.pomjidelnic_xmljidelnic, jidelna))
-        console.log(result.VFPData.pomjidelnic_xmljidelnic)
+        //console.log(result.VFPData.pomjidelnic_xmljidelnic)
       });
   });
     
     console.log(jidelna)
+    console.log("--------")
     
 });
 
@@ -70,8 +75,8 @@ function makeCal(body, jidelna) {
             start: [Number(date[0]), Number(date[1]), Number(date[2]), Number(keychain.time[0]), Number(keychain.time[1])],
             duration: { hours: 0, minutes: 30 },
             title: obed.nazev.join(),
-            description: `${obed.popis.join()}\n${obed.popis_al.join()}`,
-            location: "Jedáleň",
+            description: `${obed.popis.join()}\n${obed.popis_al.join()}\n--------\nhttps://www.strava.cz/strava/stravnik/jidelnicky?zarizeni=${jidelna}`,
+            location: pekelneRes.get(`${jidelna}j`),
             url: `https://www.strava.cz/strava/stravnik/jidelnicky?zarizeni=${jidelna}`,
             status: 'CONFIRMED',
             busyStatus: 'BUSY',
@@ -85,7 +90,7 @@ function makeCal(body, jidelna) {
         console.log(error)
         return
       }
-      console.log(value)
+      //console.log(value)
       
       return value
 }
